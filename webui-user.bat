1209f4794b602d565166d1b309a747dd8cde9ca9
Dima121287 <dima121287@MAIL.RU>
2024-01-17 13:32:53 +0300

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--xformers --lowvram --disable-safe-unpickle --autolaunch no-half-vae
+set SAFETENSORS_FAST_GPU=1
 
 call webui.bat
